92fa1399284cc7372d4878f2431bacdf3936736f
stropee <stropee@campus-eni.fr>
2024-01-29 16:58:24 +0100

    mod3demo1
    Signed-off-by: stropee <stropee@campus-eni.fr>

---
+++ mod3demo1/src/main/java/com/example/mod3demo1/MyClass.kt
@@ -0,0 +1,17 @@
+package com.example.mod3demo1
+
+fun main() {
+
+    val hello = "Hello World !"
+    println(hello)
+
+    //inférence de type la variable est typé naturellement
+    val value = 12
+    //value = 15 pas possible
+    println(value)
+
+    var truc = "Machin"
+    truc = "Bidule"
+
+    println(truc)
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,3 +17,4 @@ rootProject.name = "helloWorld"
 include(":app")
 include(":mod2demo1")
 include(":mod2demo2")
+include(":mod3demo1")
