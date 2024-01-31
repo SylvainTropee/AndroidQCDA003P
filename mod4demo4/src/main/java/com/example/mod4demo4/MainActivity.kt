b9195af754fff1bff391af33856a5bb8d27c3a5b
stropee <stropee@campus-eni.fr>
2024-01-31 14:38:15 +0100

    mod3demo4.2
    Signed-off-by: stropee <stropee@campus-eni.fr>

--- mod4demo4/src/main/java/com/example/mod4demo4/MainActivity.kt
+++ mod4demo4/src/main/java/com/example/mod4demo4/MainActivity.kt
@@ -2,9 +2,11 @@ package com.example.mod4demo4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
 import com.example.mod4demo4.databinding.ActivityMainBinding
 
+private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
@@ -18,6 +20,12 @@ class MainActivity : AppCompatActivity() {
         //binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         binding.user = User("Michel", "Drucker", 82, "michel@mail.com")
+
+
+        binding.button.setOnClickListener {
+            val user = binding.user
+            Log.i(TAG, "onCreate: " + user)
+        }
 
 
 
