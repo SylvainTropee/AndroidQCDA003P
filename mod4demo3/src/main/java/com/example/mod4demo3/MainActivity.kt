a71e23d18ee46fcd7ba4087c087e037c73ef1097
stropee <stropee@campus-eni.fr>
2024-01-31 10:29:09 +0100

    mod4demo4
    Signed-off-by: stropee <stropee@campus-eni.fr>

--- mod4demo3/src/main/java/com/example/mod4demo3/MainActivity.kt
+++ mod4demo3/src/main/java/com/example/mod4demo3/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.mod4demo3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mod4demo3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            //récupère le contenu de l'input pour le mettre dans le textview
+            binding.tvHello.text = binding.etForm.text.toString()
+        }
+
     }
 }
