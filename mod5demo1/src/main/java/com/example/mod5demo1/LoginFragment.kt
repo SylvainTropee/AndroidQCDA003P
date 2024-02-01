9c76698c8ebf52cbd3e1540ef9618b19dac24792
stropee <stropee@campus-eni.fr>
2024-02-01 10:10:51 +0100

    mod5demo1
    Signed-off-by: stropee <stropee@campus-eni.fr>

---
+++ mod5demo1/src/main/java/com/example/mod5demo1/LoginFragment.kt
@@ -0,0 +1,33 @@
+package com.example.mod5demo1
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val button = view.findViewById<Button>(R.id.button)
+
+        button.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.actionLoginToHome)
+        }
+
+
+    }
+
+}
