ba8de4b2dff7e012df22de9f9543d45a6fbffa52
stropee <stropee@campus-eni.fr>
2024-02-05 12:30:05 +0100

    mod8demo1.2
    Signed-off-by: stropee <stropee@campus-eni.fr>

--- mod8demo1/src/main/java/com/example/mod8demo1/CounterViewModel.kt
+++ mod8demo1/src/main/java/com/example/mod8demo1/CounterViewModel.kt
@@ -1,13 +1,14 @@
 package com.example.mod8demo1
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class CounterViewModel : ViewModel() {
 
-    var counter = 0
+    var counter = MutableLiveData<Int>(0)
 
-    fun increment(): Int {
-        return ++counter
+    fun increment() {
+        counter.value = counter.value?.inc()
     }
 
 }
--- mod8demo1/src/main/java/com/example/mod8demo1/MainActivity.kt
+++ mod8demo1/src/main/java/com/example/mod8demo1/MainActivity.kt
@@ -5,28 +5,30 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
+import com.example.mod8demo1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var vm: CounterViewModel
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val button = findViewById<Button>(R.id.button)
         val tv = findViewById<TextView>(R.id.tvCounter)
 
         vm = ViewModelProvider(this)[CounterViewModel::class.java]
-
-        tv.text = vm.counter.toString()
+        vm.counter.observe(this){
+            binding.vm = vm
+        }
 
         button.setOnClickListener {
-            tv.text = vm.increment().toString()
+            vm.increment()
         }
 
     }
-
-
 
 
 }
